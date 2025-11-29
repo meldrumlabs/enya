eb766cedbdba631284f372a06e69ccee3b6e58ad
Max Meldrum <11488530+Max-Meldrum@users.noreply.github.com>
2025-11-29 13:54:05 +0100

    refactor build tool/info

--- crates/enya/build.rs
+++ crates/enya/build.rs
@@ -1,17 +1,16 @@
 fn main() {
     // Export base metadata (features, rustc/llvm version, etc.).
-    re_build_tools::export_build_info_vars_for_crate(env!("CARGO_PKG_NAME"));
+    enya_build_tools::export_build_info_vars_for_crate(env!("CARGO_PKG_NAME"));
 
     // Override the git-related fields so we get useful info even when building locally.
-    if let Ok(hash) = re_build_tools::git_commit_hash() {
-        println!("cargo:rustc-env=RE_BUILD_GIT_HASH={hash}");
+    if let Ok(hash) = enya_build_tools::git_commit_hash() {
+        println!("cargo:rustc-env=ENYA_BUILD_GIT_HASH={hash}");
     }
-    if let Ok(branch) = re_build_tools::git_branch() {
-        println!("cargo:rustc-env=RE_BUILD_GIT_BRANCH={branch}");
+    if let Ok(branch) = enya_build_tools::git_branch() {
+        println!("cargo:rustc-env=ENYA_BUILD_GIT_BRANCH={branch}");
     }
 
-    // Always include a build timestamp and mark that we're outside the rerun workspace.
+    // Always include a build timestamp.
     let timestamp = chrono::Utc::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
-    println!("cargo:rustc-env=RE_BUILD_DATETIME={timestamp}");
-    println!("cargo:rustc-env=RE_BUILD_IS_IN_RERUN_WORKSPACE=no");
+    println!("cargo:rustc-env=ENYA_BUILD_DATETIME={timestamp}");
 }
